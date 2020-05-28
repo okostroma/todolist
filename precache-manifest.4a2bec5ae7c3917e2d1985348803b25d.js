a7268e9e4c7b0a491a0152757edaacc438d0ff98
Oxa <oxakost@gmail.com>
2020-05-29 00:04:56 +0300

    Updates

---
+++ precache-manifest.4a2bec5ae7c3917e2d1985348803b25d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ffd543baee048495f97042c0735ba32",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "f6b69c93facddd31cb71",
+    "url": "/todolist/static/css/main.bded4f16.chunk.css"
+  },
+  {
+    "revision": "26b78c7ad1c3fb100ac8",
+    "url": "/todolist/static/js/2.4a773b0d.chunk.js"
+  },
+  {
+    "revision": "f6b69c93facddd31cb71",
+    "url": "/todolist/static/js/main.fdd2e6d1.chunk.js"
+  },
+  {
+    "revision": "5225356577f332e6df46",
+    "url": "/todolist/static/js/runtime~main.d3bf418b.js"
+  }
+]);
